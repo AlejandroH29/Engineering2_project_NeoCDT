bb1c60903aa88d093824ae1d7c24254bbae2e050
KeFra06 <kevin_esteban.franco@uao.edu.co>
2025-10-09 12:01:33 -0500

    Crear y editar solicitud

--- back_end/src/Services/solicitudCDTService.js
+++ back_end/src/Services/solicitudCDTService.js
@@ -1,15 +1,16 @@
 import { solicitudCDT } from "../Models/solicitudCDTModel.js";
+import { Usuario } from "../Models/UsuarioModel.js";
 
 
-function calculatInteresCDT(diasInversion){
+function calcularInteresCDT(diasInversion){
     const tasaEfectiva = (1 + 0.10)**(diasInversion/365)-1;
     return tasaEfectiva;
 }
 
 function calcularGananciaCDT(montoInvertido, tasaEfectiva){
     const interes = (montoInvertido * tasaEfectiva);
-    const ganancias = montoInvertido + interes;
-    return ganancias;
+    const montoFinal = montoInvertido + interes;
+    return montoFinal;
 }
 
 function generarNumeroSolicitudAleatorio(){
@@ -17,26 +18,69 @@ function generarNumeroSolicitudAleatorio(){
 }
 
 const crearSolicitudCDT = async (solicitud) =>{
-    const numeroAleatorio = generarNumeroSolicitudAleatorio();
+
+    if (!solicitud.montoInicial || solicitud.montoInicial <= 0) {
+        throw new Error("El monto inicial debe ser mayor a 0");
+    }
+
+    let numeroAleatorio;
+    let existe = true;  
+    while (existe) {
+        numeroAleatorio = generarNumeroSolicitudAleatorio();
+        const solicitudPorNumero = await solicitudCDT.findOne({
+            where: {numeroSolicitud: numeroAleatorio}
+        })
+        existe = !!solicitudPorNumero;
+    }
     solicitud.numeroSolicitud = numeroAleatorio;
 
     solicitud.estado = "Borrador";
 
-    const solicitudPorNumero = await solicitudCDT.findOne({
-        where: { numeroSolicitud: numeroAleatorio}
-    })
-    if(solicitudPorNumero != null){
-        throw new Error("El numero de la solicitud ya existe")
-    }
-    const montoInicialSolicitud = await solicitudCDT.findOne({
-        where: {montoInicial: solicitud.montoInicial}
-    })
-    if(montoInicialSolicitud <=0){
-        throw new Error("El monto inicial debe ser mayor a 0")
-    }
+    const tasaEfectiva = calcularInteresCDT(solicitud.diasInversion);
+    const montoFinal = calcularGananciaCDT(solicitud.montoInicial, tasaEfectiva);
+
+    solicitud.tasaInteres = tasaEfectiva;
+    solicitud.montoGanancia = montoFinal;
 
     const nuevaSolicitud = await solicitudCDT.create(solicitud);
     return nuevaSolicitud;
 }
 
-export {crearSolicitudCDT}
+const estadosPermitidosPorAgente = ["enValidacion", "Aprobada", "Rechazada"]
+const estadoPermitidoPorCliente = "Cancelada"
+const actualizarSolicitudCDT = async (id, usuarioId, nuevoEstado) =>{
+
+    const solicitud = await solicitudCDT.findByPk(id);
+    if (!solicitud) {
+        throw new Error("Solicitud no encontrada");
+    }
+    
+    const usuario = await Usuario.findByPk(usuarioId);
+    if (!usuario) {
+        throw new Error("Usuario no encontrado")
+    }
+
+    if (usuario.tipo !== "Agente") {
+        if (!estadosPermitidosPorAgente.includes(nuevoEstado)) {
+            throw new Error("El estado al que vas a modificar la solicitud no es valido");
+        }
+    } else if (usuario.tipo == "Cliente") {
+        if (nuevoEstado !== estadoPermitidoPorCliente) {
+            throw new Error("El estado al que vas a modificar la solicitud no es valido para Cliente");
+        }
+    } else {
+         throw new Error("Tipo de usuario no autorizado para actuualizar el estado de la solicitud");
+    }
+
+    if (solicitud.estado == nuevoEstado){
+    throw new Error("No puedes modificar una solicitud con el mismo estado")
+  }
+
+  solicitud.estado = nuevoEstado;
+    await solicitud.save();
+
+    return solicitud;
+   
+  }
+
+export {crearSolicitudCDT, actualizarSolicitudCDT}
