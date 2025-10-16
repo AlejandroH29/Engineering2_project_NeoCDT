b1a82899477428c825e569a9fe9cc373071acff1
Samuel <sepulvedacsamuel09@gmail.com>
2025-10-16 18:10:39 -0500

    Dale jose

--- front_end/src/pages/MyRequests.jsx
+++ front_end/src/pages/MyRequests.jsx
@@ -1,10 +1,25 @@
 import {useContext, useState} from 'react'
 import { AuthContext } from '../context/AuthContext'
 import { RequestCard } from '../components/RequestCard'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 export const MyRequests = () => {
+    const navigate = useNavigate();
     const {currentUser} = useContext(AuthContext)
     const [myRequests, setMyRequests] = useState([]);
+
+    const handleEdit = () => {
+        navigate("/request-form");
+    }
+
+    const handleDelete = async () => {
+        try{
+            await axios.delete(`http://localhost:3000/eliminarSolicitudCDT/${request.numero}`)
+        }catch (error){
+            alert(error?.response?.data?.error)
+        }
+    }
 
     return (
         <div>
@@ -19,6 +34,8 @@ export const MyRequests = () => {
                             estado={request.estado}
                             plazo={request.tiempo}
                             monto={request.montoInicial}
+                            onDelete={handleDelete}
+                            onEdit={handleEdit}
                             />
                         </div>
                     ))
