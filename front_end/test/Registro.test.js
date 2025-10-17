3f661841c4a6dd16235e48de9cb6b5d46c7d2da2
joseramirez1805 <joseramirezcasas18@gmail.com>
2025-10-17 11:39:57 -0500

    Prueba unitaria render Frontend

---
+++ front_end/test/Registro.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import { Register } from '../src/pages/Register';
+
+jest.mock('react-router-dom', () => ({
+  useNavigate: () => jest.fn(),
+}));
+
+test('renders Register component', () => {
+  render(<Register />);
+  expect(screen.getByText('NeoCDT')).toBeInTheDocument();
+});
