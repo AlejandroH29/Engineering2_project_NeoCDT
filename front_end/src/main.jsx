fcf5dbbe891b7167de732082016daf6e3c09e40e
joseramirez1805 <joseramirezcasas18@gmail.com>
2025-11-07 11:04:23 -0500

    Quitar UseContext Errores general

--- front_end/src/main.jsx
+++ front_end/src/main.jsx
@@ -4,16 +4,13 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import { AppRoutes } from './routes/AppRoutes.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
-import { ErrorProvider } from './context/ErrorContext'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <StrictMode>
-      <ErrorProvider>
         <AuthProvider>
           <AppRoutes/>
         </AuthProvider>
-      </ErrorProvider>
     </StrictMode>
   </BrowserRouter>
 )
